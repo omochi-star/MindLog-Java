bfb9fcecc9ba4a8150dd667f0785e3850defb37f
omochi-star <omochim275@gmail.com>
2026-02-01 16:26:42 +0900

    Initialize Spring Boot project

---
+++ src/test/java/com/example/mindlog/MindlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
